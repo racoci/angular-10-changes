b396efe403a7aefe45852122869f8309afcfa83f
Lucas Racoci <racoci.0@gmail.com>
2025-03-23 15:16:39 -0300

    Introduction to subscribers and observables.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 
 import { CounterComponent } from './counter/counter.component';
 import { MessagesComponent } from './messages/messages.component';
+import {interval, map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,37 @@ import { MessagesComponent } from './messages/messages.component';
   styleUrl: './app.component.css',
   imports: [CounterComponent, MessagesComponent],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef)
+
+  private customInterval = new Observable<number>( (subscriber) => {
+    let k = 0;
+    setInterval(() => {
+      subscriber.next(k)
+      k++;
+    }, 1000);
+  });
+
   get debugOutput() {
     console.log('[AppComponent] "debugOutput" binding re-evaluated.');
     return 'AppComponent Component Debug Output';
+  }
+  ngOnInit() {
+    console.log('[AppComponent] "ngOnInit" binding re-evaluated.');
+
+    const subscription = this.customInterval.pipe(
+      map((it) => it * 2)
+    ).subscribe({
+      next: data => {
+        console.log(data);
+      },
+      complete: () => {
+        console.log('[AppComponent] "ngOnInit" complete.');
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe()
+    })
   }
 }
